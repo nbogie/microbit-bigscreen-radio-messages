9664c7a53888be7629dc06648c58b06a4aae151a
Neill <neillbogie@googlemail.com>
2018-12-10 17:45:28 +0000

    bridge now reports grp chg via serial

--- microbit_components/src/radio-sniffer-serial-bridge.js
+++ microbit_components/src/radio-sniffer-serial-bridge.js
@@ -7,9 +7,6 @@ function wrapGroup() {
         group = 255
     }
 }
-input.onButtonPressed(Button.A, function () {
-    incrementGroup(-1)
-})
 input.onButtonPressed(Button.B, function () {
     incrementGroup(1)
 })
@@ -17,13 +14,24 @@ radio.onReceivedString(function (receivedString) {
     serial.writeLine("" + receivedString + "|" + radio.receivedPacket(RadioPacketProperty.SerialNumber))
     led.toggle(0, 0)
 })
+function reportChangedGroup() {
+    serial.writeLine("" + `relay listens group ${group}|001`)
+    led.toggle(0, 0)
+}
+input.onButtonPressed(Button.A, function () {
+    incrementGroup(-1)
+})
 let group = 0
+basic.showString("ulisten", 50)
 dataCount = 0
+
 function incrementGroup(inc: number) {
     group += inc
     wrapGroup()
     radio.setGroup(group)
+    reportChangedGroup()
     basic.showNumber(group, 40)
+
 }
 group = 1
 radio.setGroup(group)
