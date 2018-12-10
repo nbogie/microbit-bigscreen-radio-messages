a520059f98ba513cf15c54949f32e6bd9ec48ef9
nbogie <neillbogie@googlemail.com>
2018-12-10 17:39:10 +0000

    update msg-to-send quicker,don't wait for anim to finish

--- microbit_components/src/test-sender.js
+++ microbit_components/src/test-sender.js
@@ -1,15 +1,18 @@
 let list: string[] = []
 let msg = ""
 let ix = 0
+
 input.onButtonPressed(Button.A, function () {
     ix = (ix + 1) % list.length
-    basic.showString("" + msg, 50)
-msg = list[ix]
+    msg = list[ix]
+    basic.showString("" + msg, 40)
 })
+
 input.onButtonPressed(Button.B, function () {
     radio.sendString(msg)
     led.toggle(4, 0)
 })
+
 radio.setGroup(1)
 radio.setTransmitSerialNumber(true)
 list = ["trooper", "scout", "the reaper", "hay man", "skull trooper", "dj yonder"]
